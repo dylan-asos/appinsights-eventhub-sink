bc7586681ccad0c0b7e0803105b59c2f64594f9c
Dylan Morley <dylanm@asos.com>
2018-09-26 10:52:53 +0100

    Update EventHubsSink.cs

--- src/AppInsights.EventHub.Sink.NetFramework/Telemetry/Sink/EventHubsSink.cs
+++ src/AppInsights.EventHub.Sink.NetFramework/Telemetry/Sink/EventHubsSink.cs
@@ -5,18 +5,18 @@ using Microsoft.ApplicationInsights.Extensibility;
 namespace AppInsights.EventHub.Sink.NetFramework.Telemetry.Sink
 {
     public class EventHubsSink : EventHubSinkPublisher, ITelemetryProcessor
+    {
+	protected readonly ITelemetryProcessor Next;
+
+	public EventHubsSink(EventHubSinkSettings settings, ITelemetryProcessor next) : base(settings)
 	{
-		protected readonly ITelemetryProcessor Next;
-
-		public EventHubsSink(EventHubSinkSettings settings, ITelemetryProcessor next) : base(settings)
-		{
-			Next = next;
-		}
-
-		public void Process(ITelemetry item)
-		{
-            PostEntry(item);
-			Next.Process(item);
-		}        
+	    Next = next;
 	}
+
+	public void Process(ITelemetry item)
+	{
+	    PostEntry(item);
+	    Next.Process(item);
+	}        
+    }
 }
